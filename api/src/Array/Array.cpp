ae98e759f50eb534aa94e75b60767c0f98539064
ll <2856880955@qq.com>
2025-06-30 15:20:38 +0800

    新增了Array类的bool emplace(int key,std::string val)方法

--- api/src/Array/Array.cpp
+++ api/src/Array/Array.cpp
@@ -1,9 +1,9 @@
 #include"../../include/Array/Array.h"
 namespace EWUSG{
 
-Array::Array(int capacity=100){
-    this->capacity_=capacity;
+Array::Array(int capacity=100):capacity_(capacity),size_(0){
     this->array_=new DbLinkedList*[capacity_];
+    this->prime_=this->caculateMaxPrime();   //计算当下最大的素数
 }
 
 bool Array::isElementEmpty(int pos){
@@ -12,22 +12,33 @@ bool Array::isElementEmpty(int pos){
 }
 
 int Array::primeMode(int key){
-    return key%capacity_;
+    return key%prime_;    //通过 [除留余数法]来计算“哈希桶"的位置
 }
 
 //计算最大素数
 int Array::caculateMaxPrime(){
     int maxPrime=1;
-    for(int i=1;i<capacity_;i++){
-       bool flag=false;
+    for(int i=capacity_;i>=1;i--){
+       bool flag=true;
        for(int j=2;j<i;j++){
-                    
+          if(i%j==0){
+            flag=false;
+            break;
+          }         
        }
+       if(flag)return i;
     }
 }
 
-bool Array::emplace(int key,int val){
-     int pos=this->primeMode(key);
+bool Array::emplace(int key,std::string val){
+    int pos=this->primeMode(key);           //计算“哈希桶”的下标
+    DbLinkedList*list=nullptr;
+    if(isElementEmpty(pos)){                //如果当前“哈希桶”为空
+      array_[pos]=new DbLinkedList;         //创建一个空的“哈希桶”
+    }
+    list=array_[pos];
+    list->push_back(key,val);               //插入哈希表中
+    return true;
 }
 
 
